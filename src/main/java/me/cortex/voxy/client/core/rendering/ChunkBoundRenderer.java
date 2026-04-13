a271de0e1756606ed77415805ff8435e3e535a26
lap2ka <83134094+lap2ka@users.noreply.github.com>
2026-04-13 19:53:53 +0200

    Fix SSAO, blinking, holes on chunk borders

--- src/main/java/me/cortex/voxy/client/core/rendering/ChunkBoundRenderer.java
+++ src/main/java/me/cortex/voxy/client/core/rendering/ChunkBoundRenderer.java
@@ -90,7 +90,13 @@ public class ChunkBoundRenderer {
             }
         }
 
-        if (this.chunk2idx.isEmpty() && this.addQueue.isEmpty()) return;
+        if (!this.addQueue.isEmpty()) {
+            this.addQueue.forEach(this::_addPos);//TODO: REPLACE WITH SCATTER COMPUTE
+            this.addQueue.clear();
+            UploadStream.INSTANCE.commit();
+        }
+
+        if (this.chunk2idx.isEmpty()) return;
 
         viewport.depthBoundingBuffer.clear(this.properties.inverseClearDepth());
 
@@ -156,12 +162,6 @@ public class ChunkBoundRenderer {
             glEnable(GL_DEPTH_TEST);
         }
 
-
-        if (!this.addQueue.isEmpty()) {
-            this.addQueue.forEach(this::_addPos);//TODO: REPLACE WITH SCATTER COMPUTE
-            this.addQueue.clear();
-            UploadStream.INSTANCE.commit();
-        }
     }
 
     private void _remPos(long pos) {
